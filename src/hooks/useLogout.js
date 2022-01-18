b8301c9fbfafacdcdb51e4e8a4974bc46a745edb
Daniel O'Brien <daniel@monomania.ie>
2022-01-18 22:15:53 +0000

    added pending/error handling

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,19 +1,40 @@
 import { useAuthContext } from './useAuthContext';
+import { useState, useEffect } from 'react';
 // firebse imports
 import { auth } from '../firebase/config';
 import { signOut } from '@firebase/auth';
 
 export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setisPending] = useState(false);
+
   const { dispatch } = useAuthContext();
 
-  const logout = () => {
-    signOut(auth)
-      .then(() => {
-        console.log('user signed out');
-        dispatch({ type: 'LOGOUT' });
-      })
-      .catch((err) => console.log(err.message));
-  };
+  const logout = async () => {
+    setError(null);
+    setisPending(true);
 
-  return { logout };
+    try {
+      await signOut(auth);
+      console.log('User signed out');
+      dispatch({ type: 'LOGOUT' });
+
+      if (!isCancelled) {
+        setError(null);
+        setisPending(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+      if (!isCancelled) {
+        setError(error.message);
+        setisPending(false);
+      }
+    }
+  };
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { logout, error, isPending };
 };
