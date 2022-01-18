83b6a3e29c5b569188b5ed4050a93fd6f890eea4
Daniel O'Brien <daniel@monomania.ie>
2022-01-18 22:15:16 +0000

    Added AUTH checks to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // styles & images
 import './Navbar.css';
 import Logo from '../assets/Logo.svg';
 
 export default function Navbar() {
+  const { isPending, logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <div className='navbar'>
       <ul>
@@ -12,19 +16,30 @@ export default function Navbar() {
           <img src={Logo} alt='App Logo' />
           <span>Sale Genie</span>
         </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-
-        <li className='avatar'>
-          <img src='' alt='' />
-        </li>
-        <li>
-          <button>Logout</button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li className='avatar'>
+              <img src='' alt='' />
+            </li>
+            <li>
+              {!isPending ? (
+                <button onClick={logout}>Logout</button>
+              ) : (
+                <button disabled>Logging out...</button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
