afe382fde7743f8651a976f124f67979f2bcdf2e
Daniel O'Brien <daniel@monomania.ie>
2022-01-18 20:44:20 +0000

    Created basic Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,31 @@
+import { Link } from 'react-router-dom';
+
+// styles & images
 import './Navbar.css';
+import Logo from '../assets/Logo.svg';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <ul>
+        <li className='logo'>
+          <img src={Logo} alt='App Logo' />
+          <span>Sale Genie</span>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/signup'>Signup</Link>
+        </li>
+
+        <li className='avatar'>
+          <img src='' alt='' />
+        </li>
+        <li>
+          <button>Logout</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
