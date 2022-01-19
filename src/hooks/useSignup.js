ceab0af454a0c50d5b6263a7f43e97ab13ef607e
Daniel O'Brien <daniel@monomania.ie>
2022-01-19 22:03:24 +0000

    useSignup updated

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,24 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuthContext } from './useAuthContext';
 
 // firebase imports
-import { auth } from '../firebase/config';
+import { auth, db } from '../firebase/config';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 
 export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = (email, password) => {
+  const signup = async (email, password, firstName, thumbnail) => {
     setError(null);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((response) => {
-        dispatch({ type: 'LOGIN', payload: response.user });
-      })
-      .catch((err) => {
-        setError(err.message);
+    setIsPending(true);
+
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      if (!response) {
+        throw new Error('Could not complete signup');
+      }
+
+      // generate and upload user thumbnail
+      // const uploadPath = `thumbnails/${response.user.uid}/${thumbnail.name}`;
+      // const image = await projectStorage.ref(uploadPath).put(thumbnail);
+      // const imageUrl = await image.ref.getDownloadURL();
+
+      // add display name to user
+      // await response.user.updateProfile({ displayName, photoURL: imageUrl });
+
+      // create a user document
+      await setDoc(doc(db, 'users', response.user.uid), {
+        name: firstName,
+        state: 'CA',
+        country: 'USA',
       });
+
+      dispatch({ type: 'LOGIN', payload: response.user });
+
+      // set state
+      if (!isCancelled) {
+        setIsPending(false);
+      }
+    } catch (error) {
+      console.log(error);
+      if (!isCancelled) {
+        setError(error.message);
+        setIsPending(false);
+      }
+    }
   };
 
-  return { error, signup };
+  useEffect(() => {
+    return () => {
+      setIsCancelled(true);
+    };
+  }, []);
+
+  return { error, isPending, signup };
 };
