8810dd12f5bcab480db794761be5e92860c289bd
Daniel O'Brien <daniel@monomania.ie>
2022-01-20 20:32:04 +0000

    Profile Init

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/home/Home';
 import Navbar from './components/Navbar';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
           </Routes>
         </BrowserRouter>
       )}
