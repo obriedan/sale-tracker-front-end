25c69ce2ee0f9cf9323b5346ba152ece3eab6931
Daniel O'Brien <daniel@monomania.ie>
2022-01-20 12:28:27 +0000

    added authIsReady check to app load

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Home from './pages/home/Home';
 import Navbar from './components/Navbar';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
