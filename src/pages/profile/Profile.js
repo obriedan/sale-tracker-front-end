3b68f262ab292eed43d397e28e5d48098720d069
Daniel O'Brien <daniel@monomania.ie>
2022-01-21 10:50:27 +0000

    profile page init

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,5 +1,28 @@
-import { AuthContextProvider } from '../../context/AuthContext';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useState } from 'react';
+
+import { getMessaging, getToken } from 'firebase/messaging';
+
+const publicKey =
+  'BHD6mDvdr5XP6BAELtbrYIAFuFn3iliXFAZ8GvhSIjKClq8H7lnVgQWibjZD21Oi9-rip2zLWwKInytOS-JFnMs';
 
 export default function Profile() {
-  return <div>hello</div>;
+  const messaging = getMessaging();
+  const [email, setEmail] = useState(null);
+  const [isTokenFound, setIsTokenFound] = useState(false);
+
+  const { user } = useAuthContext();
+
+  getToken(messaging, { vapidKey: publicKey }).then((currentToken) => {});
+
+  return (
+    <div className='user-details'>
+      <form>
+        <label>
+          <span>e-mail:</span>
+          <input type='text' value={email ? email : user.email} />
+        </label>
+      </form>
+    </div>
+  );
 }
